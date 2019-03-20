7e665efb69f58aac380ee0e142117d96637404bc
KROHNICK <business.kanekperez@gmail.com>
2019-03-20 14:20:05 -0700

    auth, toekn middleware, added to reg

--- routes/users/register.js
+++ routes/users/register.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../../database/dbConfig");
+const bcrypt = require("bcryptjs");
 
-router.post("/register", (req, res) => {
+const { authenticate, generateToken } = require("../../auth/authenticate");
+
+router.post("/register", authenticate, (req, res) => {
   const creds = req.body;
   db("users")
     .insert(creds)
     .then(id => {
+      const token = generateToken(user);
       res.status(201).json({
         id: id[0],
-        message: "Registered successfully"
+        message: "Registered successfully",
+        token
       });
     })
     .catch(err => {
