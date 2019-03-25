1d5d6acd83ca5d71f5f737a942f5b906df2d5d8c
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-03-25 15:55:02 -0500

    daily data post endpoint

--- routes/dailyData/dailyData.js
+++ routes/dailyData/dailyData.js
@@ -11,4 +11,17 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.post('/', async (req, res) => {
+  const { sleeptime, waketime, qos_score } = req.body
+  if (!sleeptime || !waketime || !qos_score) {
+      res.status(404).json({ message: 'Adding daily data requires a sleeptime, waketime and qos_score.' })
+  }
+  try {
+      const dailyData = await db('daily_data').insert(req.body)
+      res.status(201).json(dailyData)
+  } catch (error) {
+      res.status(500).json({ error: 'Daily data information could not be retrieved.' })
+  }
+})
+
 module.exports = router;
