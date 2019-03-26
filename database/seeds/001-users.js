9d5e89aeb0ac977620222393fa158ce4a848c180
bhumip214 <bhumip214@gmail.com>
2019-03-26 15:04:39 -0700

    updated seeds

--- database/seeds/001-users.js
+++ database/seeds/001-users.js
@@ -1,28 +1,26 @@
-var faker = require('faker');
+var faker = require("faker");
 
 var curArr = [];
 
-for(var i = 0; i < 125; i++){
-	var firstName = faker.name.firstName();
-	var lastName = faker.name.lastName();
-	var randomEmail = faker.internet.email();
-	var randomPassword = faker.internet.password();
-	
-	curArr.push({
-		email: randomEmail,
-		password: randomPassword,
-		f_name: firstName,
+for (var i = 0; i < 125; i++) {
+  var firstName = faker.name.firstName();
+  var lastName = faker.name.lastName();
+  var randomEmail = faker.internet.email();
+
+  curArr.push({
+    email: randomEmail,
+    f_name: firstName,
     l_name: lastName,
-    account_type: 'user'
-})
+    account_type: "user"
+  });
 }
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('users')
+  return knex("users")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex('users').insert(curArr);
+      return knex("users").insert(curArr);
     });
 };
