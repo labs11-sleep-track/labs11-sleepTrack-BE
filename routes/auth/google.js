a0188ae9c5a8f671c7ea2d61906ae7ee793a665d
bhumip214 <bhumip214@gmail.com>
2019-03-26 14:28:05 -0700

    able to login with google OAuth

--- routes/auth/google.js
+++ routes/auth/google.js
@@ -1,6 +1,25 @@
 const router = require("express").Router();
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const db = require("../../database/dbConfig");
+
+router.use(passport.initialize());
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db("users")
+      .where("id", "=", id)
+      .first();
+    done(null, user);
+  } catch (error) {
+    console.log(error);
+    done(error);
+  }
+});
 
 passport.use(
   new GoogleStrategy(
@@ -15,11 +34,39 @@ passport.use(
       console.log(profile._json.email);
       console.log(profile._json.given_name);
       console.log(profile._json.family_name);
+      try {
+        const user = await db("users")
+          .where({ google_id: profile.id })
+          .first();
+
+        if (user) {
+          console.log("found user", user);
+          done(null, user);
+        } else {
+          const [id] = await db("users").insert(
+            {
+              google_id: profile.id,
+              f_name: profile._json.given_name,
+              l_name: profile._json.family_name,
+              email: profile._json.email
+            },
+            "id"
+          );
+          const newUser = await db("users")
+            .where({ id })
+            .first();
+          console.log("new user created", newUser);
+          done(null, newUser);
+        }
+      } catch (error) {
+        console.log(error);
+        done(error);
+      }
     }
   )
 );
 
-// auth with google+
+// initiate google Auth
 router.get(
   "/",
   passport.authenticate("google", { scope: ["email", "profile"] })
@@ -29,7 +76,7 @@ router.get(
 // hand control to passport to use code to grab profile info
 router.get("/redirect", passport.authenticate("google"), (req, res) => {
   // res.send(req.user);
-  res.redirect("/dashboard");
+  res.redirect(process.env.GOOGLE_REDIRECT_URL);
 });
 
 module.exports = router;
