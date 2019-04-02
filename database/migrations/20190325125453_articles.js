ec05468ea02cefbc26170009a2058cdd00a1f87a
Kelli Rae Hanson <kelliraehanson@gmail.com>
2019-04-02 17:34:59 -0500

    seeds for articles

--- database/migrations/20190325125453_articles.js
+++ database/migrations/20190325125453_articles.js
@@ -2,16 +2,12 @@
 exports.up = function(knex, Promise) {
     return knex.schema.createTable('articles', tbl => {
         tbl.increments('id');
-        tbl.string('article_title', 160).notNullable();
-        tbl.text('article_contents').notNullable();
-        tbl
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
-        tbl.timestamp('create_date').defaultTo(knex.fn.now());
+        tbl.string('title', 260).notNullable();
+        tbl.string('author', 160).notNullable();
+        tbl.string('published_date', 160)
+        tbl.string('url', 260)
+        tbl.string('image', 260)
+        tbl.text('description')
     });
 };
 
